40af7986bb1a182d6b73f8b841b4dca403140214
Vineet Vora <vineet.vora123@gmail.com>
2023-11-26 11:56:18 +0530

    changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,10 +7,10 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-     domain="dev-03ifqltxbr6nn0hn.us.auth0.com"
-     clientId="RXlGXkr49Ev5MHpvAC6vKkZ4bVn11iwl"
+     domain="dev-1dwxnh60alh3zuza.us.auth0.com"
+     clientId="bHw58Gg0lz2T79NyCvMJu8o2DcWVQ9qT"
      authorizationParams={{
-      redirect_uri: "https://full-stack-real-estate-youtube-sooty.vercel.app"
+      redirect_uri: "http://localhost:5173/"
      }}
      audience="http://localhost:8000"
      scope="openid profile email"
